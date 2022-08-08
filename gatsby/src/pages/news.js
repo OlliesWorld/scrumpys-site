8dac6fe81c36717a87018a2e31388bc907021aa2
Roni <rtlockwoodwork@gmail.com>
2022-08-07 19:51:04 -0600

    fix build issues

--- gatsby/src/pages/news.js
+++ gatsby/src/pages/news.js
@@ -1,34 +1,13 @@
-// import React from 'react';
-// import { graphql } from 'gatsby';
-// import SEO from '../components/SEO';
+import React from 'react';
 
-// export default function Newspage({ data, pageContext }) {
-//   const foods = data.food.nodes;
+import SEO from '../components/SEO';
 
-//   return (
-//     <>
-//       <SEO title="The Happenings at Scrumpy&#39; Cider Bar" />
-//       <h2>What is Happening!</h2>
-//       {/* {foods.map((food) => (
-//         <div key={food.id}>
-//           <h3 className="mark">{food.name} </h3>
-//           <p>{food.description}</p>
-//         </div>
-//       ))} */}
-//     </>
-//   );
-// }
-// export const query = graphql`
-//   query NewsQuery {
-//     news: allSanityNews {
-//       nodes {
-//         name
-//         id
-//         slug {
-//           current
-//         }
-//         description
-//       }
-//     }
-//   }
-// `;
+export default function Newspage() {
+  return (
+    <>
+      <SEO title="The Happenings at Scrumpy&#39; Cider Bar" />
+      <h2>What is Happening!</h2>
+      <p>All things are here and Happening!</p>
+    </>
+  );
+}
